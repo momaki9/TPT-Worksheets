532e47d890f6156d269566e9ce5963e78c6ddc6d
momaki9 <mostafa_m9@yahoo.com>
2023-05-11 18:00:55 -0700

    added stoichiometry section

---
+++ Chemistry/stoichiometry/script.js
@@ -0,0 +1,17 @@
+const reactions = [
+    {
+        rxn: "2Na+2H2O-->2NaOH+H2",
+        coefficients: [2, 2, 2, 1],
+        molarMass: [22.989769, 18.01528, 39.997, 2.016]
+    },
+    {
+        rxn: "Cu+4HNO3-->Cu(NO3)2+2NO2+2H2O",
+        coefficients: [1, 4, 1, 2, 2],
+        molarMass: [63.546, 63.01, 187.56, 46.0055, 18.01528]
+    },
+    {
+        rxn: "AgNO3+NaCl-->AgCl+NaNO3",
+        coefficients: [1, 1, 1, 1],
+        molarMass: [169.87, 58.44, 143.32, 84.9947]
+    }
+]
