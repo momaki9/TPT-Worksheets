c7e38b4ae3a32f5dc3c90516e365c62120a12665
momaki9 <mostafa_m9@yahoo.com>
2023-08-09 19:40:07 -0700

    added ws for sigfig

---
+++ Chemistry/sigFigs/script.js
@@ -0,0 +1,24 @@
+const numbers = [95.0, 0.04, 0.08, 0.0000098, 2023, 205000, 1.5003, 6.022, 0.02001, 500.09, 920, 783.010, 40.0, 0.00000031, 22000];
+
+const divEl = document.getElementById("sig-fig");
+
+const randomValue = Math.random();
+let valueOne = numbers[0] * randomValue;
+valueOne = (Math.round(valueOne * 100) /100).toFixed(1)
+const questionOne = document.createElement("h3");
+questionOne.textContent = `1. Original value = 95.0; New value = ${valueOne}`;
+divEl.appendChild(questionOne);
+
+const random = function () {
+    for (let index = 0; index < numbers.length; index++) {
+        const element = numbers[index];
+        const modified = numbers[index] * randomValue;
+        const roundedMod = modified.toFixed(2)
+        const subEl = document.createElement("p");
+        subEl.textContent = element + " or another value is equal to " + roundedMod;
+
+        divEl.appendChild(subEl)
+    }
+}
+
+random();
