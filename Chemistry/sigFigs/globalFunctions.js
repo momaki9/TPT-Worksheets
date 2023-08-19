1ff4be83800ca7a9d5701cbca3680f6f8595f59c
momaki9 <mostafa_m9@yahoo.com>
2023-08-18 20:24:35 -0700

    saving progress

--- Chemistry/sigFigs/globalFunctions.js
+++ Chemistry/sigFigs/globalFunctions.js
@@ -23,17 +23,24 @@ const generateNum = function (num1, num2, inc, dec) {
 
 // write a function that adds or subtracts two numbers and return the correct number of decimals
 // be sure to treat num1 and num2 as strings
-const addOrSubtract = function(num1, num2){
+const addOrSubtract = function(num1, num2, operation){
     var firstNum = Number(num1);
     var secondNum = Number(num2);
-    var sum = Number(firstNum) + Number(secondNum)
-    console.log(sum)
-    // if (!Number.isInteger(num1) && !Number.isInteger(num2)){
-        console.log(num1.toString().split('.')[1].length);
-        console.log(num2.toString().split('.')[1].length);
-    // }
+    var sum = 0;
+    if (operation == "add") {
+        sum = Number(firstNum) + Number(secondNum);
+    } else if (operation == "subtract") {
+        sum = Number(firstNum) - Number(secondNum);
+    };
+    var decimal1 = num1.toString().split('.')[1].length;
+    var decimal2 = num2.toString().split('.')[1].length;
+    var leastDecimal = Math.min(decimal1, decimal2)
+    var decimalHelper = 10**leastDecimal;
+    var answer = Math.round(sum * decimalHelper)/decimalHelper
+    answer = answer.toFixed(leastDecimal);
+    return answer;
 };
 
-addOrSubtract("4.20198", "3.001")
+console.log(addOrSubtract("4.051", "0.0094", "add"));
 
 
