34c6a995db34b4679707388c1ea8d51437565f2a
momaki9 <mostafa_m9@yahoo.com>
2023-08-19 10:07:22 -0700

    created function that selects a random element from an array

--- Chemistry/sigFigs/globalFunctions.js
+++ Chemistry/sigFigs/globalFunctions.js
@@ -1,6 +1,6 @@
 
 // write a function that generates a list of numbers from num1 to num2, given an incrementor (inc) and number of decimals
-const generateNum = function (num1, num2, inc, dec) {
+const generateNumArr = function (num1, num2, inc, dec) {
     let numArray = [];
     if (num1 == num2) {
         return;
@@ -14,11 +14,21 @@ const generateNum = function (num1, num2, inc, dec) {
             numArray.push(i.toFixed(dec));
         }
     };
-
-    console.log(numArray)
+    return numArray;
 };
 
-// generateNum(15.524, 15.25, 0.001, 3);
+generateNumArr(19, 21, 0.1, 1);
+
+// write a function that randomly returns one value from an array (array given as param)
+const selectNum = function(arr) {
+    var randomValue = arr[Math.floor(Math.random() * arr.length)];
+    console.log(randomValue);
+    return randomValue;
+};
+
+var testArr = generateNumArr(11, 12, 0.1, 1);
+console.log(testArr)
+console.log(selectNum(testArr))
 
 
 // write a function that adds or subtracts two numbers and return the correct number of decimals
@@ -58,12 +68,12 @@ const multiplyOrDivide = function (num1, num2, operation) {
     var decimal2 = num2.toString().split('.')[1].length;
     var value2 = num2.toString().split('.')[0].length;
     var digits2 = decimal2 + value2;
-    console.log(`num of sigfigs in first value is ${digits1} and in second value is ${digits2}`)
+    // console.log(`num of sigfigs in first value is ${digits1} and in second value is ${digits2}`)
     var leastSigFig = Math.min(digits1, digits2)
     var value3 = total.toString().split('.')[0].length;
     var decimalInAns = leastSigFig - value3;
-    console.log(leastSigFig)
-    console.log(decimalInAns)
+    // console.log(leastSigFig)
+    // console.log(decimalInAns)
     var decimalHelper = 10 ** decimalInAns;
     var answer = Math.round(total * decimalHelper) / decimalHelper
     answer = answer.toFixed(decimalInAns);
