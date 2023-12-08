59d6d5880b747a70d536314e95f7b677d01ac098
momaki9 <mostafa_m9@yahoo.com>
2023-12-07 21:12:35 -0800

    saving progress

--- Chemistry/00-Data/combustion.js
+++ Chemistry/00-Data/combustion.js
@@ -1,4 +1,4 @@
-const unknownCompounds = [
+const unknownCompounds = 
     {
         emFormula: "C<sub>3</sub>H<sub>4</sub>O<sub>3</sub>",
         molFormula: "",
@@ -9,20 +9,20 @@ const unknownCompounds = [
             mm: [12.011, 1.0079, 15.999]
         }
     }
-];
+
 
 // glucose combustion as an example
-const combustionEquations = 
-    {
-        eqn: "C6H12O6 + 6O2 => 6H2O + 6CO2",
-        compMM: 180.1548,
-        coef: 1,
-        prods: {
-            prod: ["CO2", "H2O"],
-            mm: [44.009, 18.015],
-            coef: [6, 6]
-        }
+const combustionEquations =
+{
+    eqn: "C6H12O6 + 6O2 => 6H2O + 6CO2",
+    compMM: 180.1548,
+    coef: 1,
+    prods: {
+        prod: ["CO2", "H2O"],
+        mm: [44.009, 18.015],
+        coef: [6, 6]
     }
+}
 
 
 const generateComubstionMasses = function (mass, mm, mm1, mm2, coef, coef1, coef2) {
@@ -32,6 +32,20 @@ const generateComubstionMasses = function (mass, mm, mm1, mm2, coef, coef1, coef
 
     return [massP1, massP2]
 };
+
+const combustionProbSolver = (m, m1, m2, mm1, mm2, coef1, coef2, mmC, mmH, mmO) => {
+    const molC = (m1 / mm1) * coef1;
+    const massC = molC * mmC;
+    const molH = (m2 / mm2) * coef2;
+    const massH = molH * mmH;
+    const massO = m - (massC + massH);
+    const molO = massO / mmO;
+    const smallest = Math.min(molC, molH, molO);
+    const subC = molC / smallest;
+    const subH = molH / smallest;
+    const subO = molO / smallest;
+    return `C ${subC.toFixed(0)} H ${subH.toFixed(0)} O${subO}`;
+}
 
 const generateNumArr = function (num1, num2, inc, dec) {
     let numArray = [];
--- Chemistry/03-Chemical_Compounds/combustion_analysis/script.js
+++ Chemistry/03-Chemical_Compounds/combustion_analysis/script.js
@@ -4,7 +4,7 @@ const contentElm = document.getElementById("content");
 const massValueArray = generateNumArr(0.2, 0.3, 0.001, 3);
 const mass = selectElm(massValueArray);
 
-const productMassValues = generateComubstionMasses(mass,combustionEquations.compMM,combustionEquations.prods.mm[0],combustionEquations.prods.mm[1],combustionEquations.coef,combustionEquations.prods.coef[0],combustionEquations.prods.coef[1]);
+const productMassValues = generateComubstionMasses(mass, combustionEquations.compMM, combustionEquations.prods.mm[0], combustionEquations.prods.mm[1], combustionEquations.coef, combustionEquations.prods.coef[0], combustionEquations.prods.coef[1]);
 
 console.log(productMassValues)
 
@@ -12,3 +12,8 @@ const massProd1 = productMassValues[0];
 const massProd2 = productMassValues[1];
 
 questionTextElm.innerHTML = `Combustion of a ${mass}-g sample of a compound containing carbon, hydrogen, and oxygen yields ${massProd1.toFixed(4)} g CO<sub>2</sub> and ${massProd2.toFixed(4)} g H<sub>2</sub>O. What is the emppirical formula of this compound?`;
+
+//m, m1, m2, mm1, mm2, coef1, coef2, mmC, mmH, mmO
+const htmlAns = combustionProbSolver(mass, massProd1, massProd2, combustionEquations.prods.mm[0], combustionEquations.prods.mm[1],1,2, unknownCompounds.elms.mm[0], unknownCompounds.elms.mm[1], unknownCompounds.elms.mm[2]);
+
+contentElm.innerHTML = htmlAns;
